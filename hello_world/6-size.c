86552a714300833505ad8eb80aaa2fb721f875c9
ChrisOxan <5760@holbertonstudents.com>
2022-09-28 05:18:23 -0700

    my first commit

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -7,16 +7,11 @@
   *
   */
 int main(void)
-	char _chat
-	int _int
-	long int _long int
-	long long int _long long int
-	float _float
 {
-	printf("Size of char: %zu" byte(s), sizeof(_char));
-	printf("Size of int: %zu" byte(s), sizeof(_int));
-	printf("Size of long int: %zu" byte(s), sizeof(_long int));
-	printf("Size of long long int: %zu" byte(s), sizeof(_long long int));
-	printf("Size of flout: %zu" byte(s), sizeof(_float));
+	printf("Size of char: %zu byte(s)", sizeof(char));
+	printf("Size of int: %zu byte(s)", sizeof(int));
+	printf("Size of long int: %zu byte(s)", sizeof(long int));
+	printf("Size of long long int: %zu byte(s)", sizeof(long long int));
+	printf("Size of flout: %zu byte(s)", sizeof(float));
 	return (0);
 }
