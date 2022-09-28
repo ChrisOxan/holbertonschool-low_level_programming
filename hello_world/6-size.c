e5369e8d99974225ddff1d623bde2a0cf4edf3d3
ChrisOxan <5760@holbertonstudents.com>
2022-09-28 05:12:01 -0700

    my first commit

---
+++ hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  *
+  * Return: Always 0 (Success)
+  *
+  */
+int main(void)
+	char _chat
+	int _int
+	long int _long int
+	long long int _long long int
+	float _float
+{
+	printf("Size of char: %zu" byte(s), sizeof(_char));
+	printf("Size of int: %zu" byte(s), sizeof(_int));
+	printf("Size of long int: %zu" byte(s), sizeof(_long int));
+	printf("Size of long long int: %zu" byte(s), sizeof(_long long int));
+	printf("Size of flout: %zu" byte(s), sizeof(_float));
+	return (0);
+}
