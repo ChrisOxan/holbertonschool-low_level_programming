bd3605509611a88b9a61239b5f23fde1386b4445
ChrisOxan <5760@holbertonstudents.com>
2022-09-29 07:53:24 -0700

    C p2 Tasks 6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+	int b10;
+
+	for (b10 = 48; b10 <= 57; b10++)
+	{
+		putchar(b10);
+	}
+		putchar('\n');
+		return (0);
+}
