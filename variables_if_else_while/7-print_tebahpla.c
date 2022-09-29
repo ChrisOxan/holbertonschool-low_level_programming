2e1b4d4483fd94c41ab775bbb26b98ca11c1c1ff
ChrisOxan <5760@holbertonstudents.com>
2022-09-29 08:37:41 -0700

    C p2 Tasks 7

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+	char AR;
+
+	for (AR = 'z' ; AR >= 'a'; AR--)
+	{
+		putchar(AR);
+	}
+		putchar('\n');
+		return (0);
+}
