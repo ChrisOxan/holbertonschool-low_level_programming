233d3eca4481d8b9d6b81adf891089aedc944c7d
ChrisOxan <5760@holbertonstudents.com>
2022-10-02 15:58:30 -0700

    C p3 Tasks 8

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Entry point
+ *
+ * Return: 0
+ */
+void jack_bauer(void)
+{
+	int hora;
+	int min;
+
+	for (hora = 0; hora < 24; hora++)
+	{
+		for (min = 0; min < 60; min++)
+		{
+			_putchar((hora / 10) + '0');
+			_putchar((hora % 10) + '0');
+			_putchar(':');
+			_putchar((min / 10) + '0');
+			_putchar((min % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
