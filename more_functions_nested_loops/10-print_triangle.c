d9aaca3bda98111ae4beda41fbfc8cd08cec5004
ChrisOxan <5760@holbertonstudents.com>
2022-10-05 16:17:09 -0700

    C p5 Tasks 10

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * print_triangle - Retry point
+ *
+ * @size: Write a function that prints a triangle, followed by a new line.
+ *
+ * Return: 0
+ */
+
+void print_triangle(int size)
+{
+	int a;
+	int b;
+
+
+	if (size > 0)
+	{
+		for (a = 1; a < size; a++)
+		{
+			for (b = size - 1; b >= 0; b--)
+			{
+				if (b > a)
+				{
+						_putchar(' ');
+				}
+				else if (b <= a)
+				{
+					_putchar('#');
+				}
+			}
+			_putchar(10);
+		}
+	}
+	else if (size <= 0)
+	{
+		_putchar(10);
+	}
+}
+
