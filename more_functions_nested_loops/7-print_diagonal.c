e7a8b41488421e44e0fb64e5c9f02d41e2fef857
ChrisOxan <5760@holbertonstudents.com>
2022-10-05 05:35:37 -0700

    C p5 Tasks 7

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -16,10 +16,15 @@ void print_diagonal(int n)
 		for (a = 0; a < n; a++)
 		{
 			for (b = 0; b < a; b++)
-			_putchar(' ');
+			{
+				_putchar(' ');
+			}
+			_putchar(92);
+			_putchar('\n');
 		}
-		_putchar(92);
+	}
+	else
+	{
 		_putchar('\n');
 	}
-	_putchar('\n');
 }
