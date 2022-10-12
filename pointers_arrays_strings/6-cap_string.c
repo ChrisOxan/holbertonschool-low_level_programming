9e2d1c94b492a87a5e08c75b2336001afa8f527a
ChrisOxan <5760@holbertonstudents.com>
2022-10-12 08:52:56 -0700

    Pointers p2 Tasks 6

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -3,34 +3,36 @@
 /**
  * cap_string - Expect the best. Prepare for the worst. Capitalize on what come
  *
- * @str: capitalizes all words of a string.
+ * @s: capitalizes all words of a string.
  *
  * Return: Always 0 (Success)
  */
 
-char *cap_string(char *str)
+char *cap_string(char *s)
 {
-	int index = 0;
+	int i = 0;
 
-	while (str[++index])
+	while (*(s + i) != '\0')
 	{
-		while (!(str[index] >= 'a' && str[index] <= 'z'))
-			index++;
-
-		if (str[index - 1] == ' ' ||
-				str[index - 1] == '\t' ||
-				str[index - 1] == '\n' ||
-				str[index - 1] == ',' ||
-				str[index - 1] == ';' ||
-				str[index - 1] == '.' ||
-				str[index - 1] == '!' ||
-				str[index - 1] == '?' ||
-				str[index - 1] == '"' ||
-				str[index - 1] == '(' ||
-				str[index - 1] == ')' ||
-				str[index - 1] == '{' ||
-				str[index - 1] == '}')
-			str[index] -= 32;
+		if (i == 0 && (*(s + i) >= 97 && *(s + i) <= 122))
+		{
+			*(s + i) = *(s + i) - ' ';
+			i++;
+		}
+		if (*(s + i) == ' ' || *(s + i) == '\n' || *(s + i) == '\t'
+		    || *(s + i) == ',' || *(s + i) == ';' || *(s + i) == '!'
+		    || *(s + i) == '?' || *(s + i) == '"' || *(s + i) == '('
+		    || *(s + i) == ')' || *(s + i) == '{' || *(s + i) == '}'
+		    || *(s + i) == '.')
+		{
+			i++;
+			if (*(s + i) >= 97 && *(s + i) <= 122)
+			{
+				*(s + i) = *(s + i) - ' ';
+			}
+		}
+		else
+			i++;
 	}
-	return (str);
+	return (s);
 }
