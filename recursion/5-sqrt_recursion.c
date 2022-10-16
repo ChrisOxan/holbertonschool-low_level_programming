ebb514cadd3e20d2f6eda92e6ba1a803d4e3e34f
ChrisOxan <5760@holbertonstudents.com>
2022-10-16 14:40:40 -0700

    Recursion p1 Tasks 5

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,32 +1,38 @@
 #include "main.h"
 
 /**
-* _sqrt_recursion - returns the natural square root of a number
-* @n: number to be used
-*
-* Return: the square root of n.
-*/
+ * _sqrt_recursion - function that returns the natural
+ * square root of a number
+ * @n: input number
+ *
+ * Return: int
+ */
 
 int _sqrt_recursion(int n)
 {
-	if (n == 0 || n == 1)
-		return (n);
-	return (_sqrt(0, n));
+	if (n == 0)
+		return (0);
+	if (n == 1)
+		return (1);
+	if (n < 0)
+		return (-1);
+	return (getNumSqr(n, 1));
 }
 
 /**
- * _sqrt - returns the square root of a number
- * @n: test number
- * @x: squared number
+ * getNumSqr - function that gets de num of sqroot
  *
- * Return: the square root of n
+ * @num: input number
+ * @i: Counter variable
+ * Return: int
  */
 
-int _sqrt(int n, int x)
+int getNumSqr(int num, int i)
 {
-	if (n > x / 2)
+	if (num == (i * i))
+		return (i);
+	else if (num > (i * i))
+		return (getNumSqr(num, i + 1));
+	else
 		return (-1);
-	else if (n * n == x)
-		return (n);
-	return (_sqrt(n + 1, x));
 }
